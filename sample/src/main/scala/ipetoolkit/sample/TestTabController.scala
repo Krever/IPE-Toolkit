53b5ed873f86e97173bfe5fa5e9ea3463372cbd3
humblehound <dejtabejz@gmail.com>
2015-09-26 18:18:15 +0200

    Everything works and it sucks

---
+++ sample/src/main/scala/ipetoolkit/sample/TestTabController.scala
@@ -0,0 +1,12 @@
+package ipetoolkit.sample
+
+import ipetoolkit.workspace.{WorkspaceEntry, DetailsController}
+
+/**
+ * Created by humblehound on 26.09.15.
+ */
+class TestTabController extends DetailsController{
+  override def setModel(workspaceEntry: WorkspaceEntry): Unit = {
+
+  }
+}
